823cd9351f59062cdc5dd458657835079793bd95
mbpro15-2015 <mbpro15-2015@git>
2022-07-26 22:55:00 -0400

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps }) {
 			serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
 		*/
 		<MoralisProvider
-			appId="vBlcpOPfGZSL0JZnKjTxGjbbDJFzugdObEnbHZ6k"
-			serverUrl="https://eq0dfzroki0h.usemoralis.com:2053/server"
+			appId="LG1i6ubtfvtOS1Or0WImq4yhNuRO5CSgKlWXp7XK"
+			serverUrl="https://nfyh8gtvf07i.usemoralis.com:2053/server"
 		>
 			<Component {...pageProps} />
 		</MoralisProvider>
